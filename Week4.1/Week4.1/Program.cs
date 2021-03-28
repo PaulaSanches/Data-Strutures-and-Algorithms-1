992c1d4f010dad40a7afbf637b5505cd4d985d82
PaulaSanches <paula.correabarrientos@gmail.com>
2021-03-28 17:51:46 +1300

    Initial commit

---
+++ Week4.1/Week4.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Week4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nBubble Sort:");
+            Test.RunTest(Sort.BubbleSort);
+            Console.WriteLine("\n\nInsertion Sort:");
+            Test.RunTest(Sort.InsertionSort);
+            Console.WriteLine("\n\nSelection Sort:");
+            Test.RunTest(Sort.SelectionSort);
+            Console.WriteLine("\n\nQuicksort:");
+            Test.RunTest(Sort.Quicksort);
+        }
+
+    }
+}
+  
+
+ 
+
+ 
